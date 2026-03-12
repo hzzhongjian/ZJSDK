d6fe7109228dd6932e7b673cf17bec46ce41c5fe
hzzhongjian <opentwo@hzzhongjian.cn>
2026-03-12 12:01:58 +0800

    修复已知问题

---
+++ ZJSDK/ZJSDKModuleWangMai/ZJ_AdWangMaiSDK.framework/Headers/ZJ_AdWangMaiSDK.h
@@ -0,0 +1,39 @@
+//
+//  ZJ_AdWangMaiSDK.h
+//  ZJ_AdWangMaiSDK
+//
+//  Created by 周泽浩 on 2023/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZJ_AdWangMaiSDK.
+FOUNDATION_EXPORT double AdWangMaiSDKVersionNumber;
+
+//! Project version string for ZJ_AdWangMaiSDK.
+FOUNDATION_EXPORT const unsigned char AdWangMaiSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZJ_AdWangMaiSDK/PublicHeader.h>
+
+
+#import <ZJ_AdWangMaiSDK/ZJ_AdWangMaiSDK.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdWangMaiSDk.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdSplashAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdSplashDelegate.h>
+
+#import <ZJ_AdWangMaiSDK/ZJ_XAdRewardedVideoAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdRewardedVideoDelegate.h>
+
+#import <ZJ_AdWangMaiSDK/ZJ_XAdInterstitialAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdInterstitialDelegate.h>
+
+#import <ZJ_AdWangMaiSDK/ZJ_XAdNativeExpressAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdNativeExpressDelegete.h>
+
+#import <ZJ_AdWangMaiSDK/ZJ_XAdNativeAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdNativeAdResponse.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdNativeDelegete.h>
+
+#import <ZJ_AdWangMaiSDK/ZJ_XAdWMNativeAd.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdWMNativeAdData.h>
+#import <ZJ_AdWangMaiSDK/ZJ_XAdWMNativeAdDelegate.h>
---
+++ ZJSDK/ZJSDKModuleWangMai/ZJ_AdWangMaiSDK.framework/Headers/ZJ_XAdWMNativeAdDelegate.h
@@ -0,0 +1,36 @@
+//
+//  ZJ_XAdWMNativeAdDelegate.h
+//  ZJ_AdWangMaiSDK
+//
+//  Created by xiang shi on 2024/9/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class ZJ_XAdWMNativeAdData;
+@protocol ZJ_XAdWMNativeAdDelegate <NSObject>
+
+/// 发起请求
+- (void)wmNativeAdOnRequest;
+
+/// 请求成功
+/// @param natives 广告物料
+- (void)wmNativeAdOnReady:(NSArray<ZJ_XAdWMNativeAdData *> *)natives;
+
+/// 请求失败
+/// @param error 错误信息
+- (void)wmNativeAdOnError:(NSError *)error;
+
+/// 广告展示
+- (void)wmNativeAdOnShow;
+
+/// 广告点击
+- (void)wmNativeAdOnClick;
+
+/// 广告详情页关闭
+- (void)wmNativeAdDidCloseOtherController;
+
+@end
+
+NS_ASSUME_NONNULL_END
